ead4ba29ed1c99fc58539827e6900aedaa805304
Kane Mathis <admin@kanemathis.com>
2022-09-12 13:51:25 -0400

    commit tree b14ba2fbdeb67ddce2bc25e71c60e428607d3836

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,54 @@
+require'pry'
 
+class CashRegister
+   
+    attr_accessor:total
+    
+    def items
+        @itemsArray
+    end
+
+    def initialize(discount=0)
+        @itemsArray=[]
+        @discount=discount
+        @total=0
+        @lastTransaction=0
+    end
+
+    def discount
+        @discount
+    end
+
+    def total
+        @total
+    end
+
+    def apply_discount
+        if @discount==0
+        "There is no discount to apply."
+        else
+        @total-=@total*discount/100
+        "After the discount, the total comes to $#{@total}."
+        end
+    end
+    
+    def add_item(title, price, quantity=1)
+        quantity.times{@itemsArray<<(title)}
+       @total+=price*quantity
+       @lastTransaction=price*quantity
+    end
+
+    def void_last_transaction
+        @total-=@lastTransaction
+    end
+end
+
+
+testreg=CashRegister.new
+binding.pry
+    # testreg.add_item("Lucky Charms", 4.5)
+     
+    #   testreg.add_item("Ritz Crackers", 5.0)
+ 
+    #   testreg.add_item("Justin's Peanut Butter Cups", 2.50, 2)
+    
